7fc422c433e8191b3df86545b23f2d864be3630a
JinHaoIsHere <horace_kim@outlook.com>
2020-06-26 20:38:36 -0700

    new repo

---
+++ burger/src/components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Logo.module.css';
+import burgerLogo from '../../assets/image.png';
+const logo = (props)=>(
+    <div className={classes.Logo} style={{height: props.height}}>
+        <img src={burgerLogo} alt="MyBurger"></img>
+    </div>
+)
+
+export default logo;
