d890854a7bac0cb8b318187fa04c2f52a20e4935
JinHaoIsHere <horace_kim@outlook.com>
2020-07-01 03:14:53 -0700

    complete task

--- routing--assignment-problem/src/containers/Course/Course.js
+++ routing--assignment-problem/src/containers/Course/Course.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 
 class Course extends Component {
-    render () {
+    state = {
+        title: 'OURSE_T',
+        id: 'ID'
+    }
+    componentDidMount() {
+        this.update();
+        // title=query.entries[1];
+        //this.setState({title: query.entries()[1]});
+    }
+    componentDidUpdate(){
+        this.update();
+    }
+    update(){
+        console.log(this.props.match.params.redirectParam);
+        const query = new URLSearchParams(this.props.location.search);
+        for (let param of query.entries()) {
+            console.log(param); // yields ['start', '5']
+            if (param[0] === 'title' && param[1]!==this.state.title) {
+                this.setState({ title: param[1] });
+            }
+
+        }
+
+    }
+    render() {
         return (
             <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
+                <h1>{this.state.title}</h1>
+                <p>You selected the Course with ID:{this.state.id}</p>
             </div>
         );
     }
--- routing--assignment-problem/src/containers/Courses/Courses.js
+++ routing--assignment-problem/src/containers/Courses/Courses.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 
 import './Courses.css';
-
+import Course from '../Course/Course';
+import { Route, Link } from 'react-router-dom'
 class Courses extends Component {
     state = {
         courses: [
@@ -11,17 +12,27 @@ class Courses extends Component {
         ]
     }
 
-    render () {
+    render() {
         return (
             <div>
                 <h1>Amazing Udemy Courses</h1>
                 <section className="Courses">
                     {
-                        this.state.courses.map( course => {
-                            return <article className="Course" key={course.id}>{course.title}</article>;
-                        } )
+                        this.state.courses.map(course => {
+                            return (
+                                    <article className="Course" key={course.id}>
+                                        <Link to={{
+                                            pathname: '/courses/' + course.id,
+                                            search: '?title=' + course.title + '&id=' + course.id,
+                                        }}>
+                                            {course.title}
+                                        </Link>
+                                    </article>
+                            )
+                        })
                     }
                 </section>
+                <Route path='/courses/:id' component={Course}></Route>
             </div>
         );
     }
